e9b408f8211d588b5c73700c0340716f3ea9bb03
Hema Sundar Sai Ratnala <hemasundarsairatnala@gmail.com>
2025-06-19 12:17:24 +0530

    Delete java programs directory

--- java programs/Do_While.java
+++
@@ -1,9 +0,0 @@
-class Do_While{
-	public static void main(String[] args){
-		int i=0;
-		do{
-			i++;
-			System.out.println(i);
-		}while(i<10);
-	}
-}
--- java programs/While.java
+++
@@ -1,9 +0,0 @@
-class While{
-	public static void main(String[] args){
-		int i=0;
-		while(i<20){
-			i++;
-			System.out.println(i);
-		}
-	}
-}
