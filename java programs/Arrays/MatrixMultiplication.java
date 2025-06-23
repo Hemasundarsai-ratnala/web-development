6106c4f123d22fe3b345a97fe6c29490cd872922
Hema Sundar Sai Ratnala <hemasundarsairatnala@gmail.com>
2025-06-23 13:11:38 +0530

    Update MatrixMultiplication.java

--- java programs/Arrays/MatrixMultiplication.java
+++ java programs/Arrays/MatrixMultiplication.java
@@ -1,6 +1,6 @@
 public class MatrixMultiplication {
     public static void main(String[] args) {
-        int[][] a = {{1, 2}, {3, 4}};
+        int[][] a = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         int[][] b = {{2, 0}, {1, 2}};
         int[][] result = new int[2][2];
 
